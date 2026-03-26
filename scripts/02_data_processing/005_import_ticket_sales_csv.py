4e5e3249db62a65e51dd12b92ff9fcee6aaccf38
Reda Serhir <ton_mail_github@exemple.com>
2026-03-26 15:55:28 +0100

    Refactor import ticket sales CSV script

--- scripts/02_data_processing/005_import_ticket_sales_csv.py
+++ scripts/02_data_processing/005_import_ticket_sales_csv.py
@@ -1,12 +1,31 @@
 import csv
 
-file_name = "data/raw/ticket_sales.csv"
 
-print("IMPORT TICKET SALES CSV")
-print("-----------------------")
+def read_ticket_sales_csv(file_name):
+    ticket_sales = []
 
-with open(file_name, "r") as file:
-    csv_reader = csv.DictReader(file)
+    with open(file_name, "r") as file:
+        csv_reader = csv.DictReader(file)
 
-    for row in csv_reader:
+        for row in csv_reader:
+            ticket_sales.append(row)
+
+    return ticket_sales
+
+
+def print_ticket_sales_rows(ticket_sales):
+    print("IMPORT TICKET SALES CSV")
+    print("-----------------------")
+
+    for row in ticket_sales:
         print(row)
+
+
+def main():
+    file_name = "data/raw/ticket_sales.csv"
+    ticket_sales = read_ticket_sales_csv(file_name)
+    print_ticket_sales_rows(ticket_sales)
+
+
+if __name__ == "__main__":
+    main()
